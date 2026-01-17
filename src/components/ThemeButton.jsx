563179b4120fd82bad5a6095f31fb4fba1ee9ecd
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-18 01:37:30 +0800

    feat(styles): apply Tailwind CSS to ThemeButton component

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -1,5 +1,3 @@
-import styles from "./theme-button.module.css";
-
 const ThemeButton = ({ onThemeChange, theme }) => {
   const isLightTheme = theme === "light";
   const isPressed = !isLightTheme;
@@ -7,7 +5,7 @@ const ThemeButton = ({ onThemeChange, theme }) => {
 
   return (
     <button
-      className={`${styles.button} ${!isLightTheme && styles.active}`}
+      className={`relative flex h-6 w-10 cursor-pointer items-center rounded-full border border-solid border-(--BORDER-COLOR-THEME-BUTTON) bg-(--BGCOLOR-THEME-BUTTON) after:absolute after:top-[0.225rem] after:bottom-[0.225rem] after:left-0 after:w-4 after:transform-[translateX(0.2rem)] after:rounded-full after:bg-(--BGCOLOR-THEME-BUTTON-AFTER) after:content-[""] motion-safe:transition-colors motion-safe:duration-300 motion-safe:ease-in-out motion-safe:after:[transition-property:transform,background-color] motion-safe:after:duration-[150ms,300ms] motion-safe:after:ease-in-out ${isPressed && "after:transform-[translateX(1.1rem)]"}`}
       type="button"
       aria-label={label}
       aria-pressed={isPressed}

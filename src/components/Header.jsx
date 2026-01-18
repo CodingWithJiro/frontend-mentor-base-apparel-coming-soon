d840ff2b001d1a9cb281d0a3dc7cb9ce4e688f56
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-18 22:19:48 +0800

    refactor(components): remove SkipLink and HeroImage in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,13 @@
 import CompanyLogo from "./CompanyLogo";
 import HeaderTitle from "./HeaderTitle";
-import HeroImage from "./HeroImage";
-import SkipLink from "./SkipLink";
 import Theme from "./Theme";
 
 const Header = () => {
   return (
     <header>
-      <SkipLink />
       <HeaderTitle />
       <CompanyLogo />
       <Theme />
-      <HeroImage />
     </header>
   );
 };

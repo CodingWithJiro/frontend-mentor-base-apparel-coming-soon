9b8285216d29478e045628ee017e703115a7bf78
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-18 22:20:41 +0800

    feat(components): update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import CompanyLogo from "./components/CompanyLogo";
-import Theme from "./components/Theme";
+import Header from "./components/Header";
+import HeroImage from "./components/HeroImage";
+import SkipLink from "./components/SkipLink";
 
 const App = () => {
   return (
     <>
-      <Theme />
-      <CompanyLogo />
+      <SkipLink />
+      <Header />
+      <HeroImage />
     </>
   );
 };

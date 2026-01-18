f569b5dc5c78e870fd0881e303f76dd88ba31d82
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-18 22:44:28 +0800

    feat(components): create MainText component

---
+++ src/components/MainText.jsx
@@ -0,0 +1,16 @@
+const MainText = () => {
+  return (
+    <>
+      <h2 className="uppercase">
+        <span>We're</span> coming soon
+      </h2>
+      <p>
+        Hello fellow shoppers! We're currently building our new fashion store.
+        Add your email below to stay up-to-date with announcements and our
+        launch deals.
+      </p>
+    </>
+  );
+};
+
+export default MainText;

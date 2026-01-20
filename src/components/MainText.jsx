f71898d45dd6cced0ade82e41ff2e303118e1120
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-21 00:27:16 +0800

    feat(styles): style MainText component

--- src/components/MainText.jsx
+++ src/components/MainText.jsx
@@ -1,10 +1,10 @@
 const MainText = () => {
   return (
     <>
-      <h2 className="uppercase">
-        <span>We're</span> coming soon
+      <h2 className="mb-4 px-4 text-center text-[2.5rem]/[1.05] font-semibold tracking-[0.275em] text-(--GREY-900) uppercase">
+        <span className="font-light text-(--PINK-400)">We're</span> coming soon
       </h2>
-      <p>
+      <p className="mb-8.5 px-9.5 text-center text-[0.885rem] font-normal text-(--PINK-400)">
         Hello fellow shoppers! We're currently building our new fashion store.
         Add your email below to stay up-to-date with announcements and our
         launch deals.

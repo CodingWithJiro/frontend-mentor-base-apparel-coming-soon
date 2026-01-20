b7b412c08b86e3852a4f34375b23639c5fcee488
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-21 00:17:54 +0800

    feat(styles): add mobile styling to HeroImage component

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -1,11 +1,15 @@
 const HeroImage = () => {
   return (
-    <picture>
+    <picture className="mb-16 w-full">
       <source
         media="(min-width: 48rem)"
         srcSet="/src/assets/img/hero-desktop_610x800.jpg"
       />
-      <img src="/src/assets/img/hero-mobile_375x250.jpg" alt="" />
+      <img
+        className="w-full"
+        src="/src/assets/img/hero-mobile_375x250.jpg"
+        alt=""
+      />
     </picture>
   );
 };

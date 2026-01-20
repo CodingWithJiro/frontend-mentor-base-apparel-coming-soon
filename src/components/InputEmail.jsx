b0908e887dab4eda7a7a7a395faef0c6cc8e8134
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-21 00:26:25 +0800

    feat(styles): style and layout InputEmail component
    I also added conditional classes depending on Form status

--- src/components/InputEmail.jsx
+++ src/components/InputEmail.jsx
@@ -1,10 +1,27 @@
 const InputEmail = ({ onChange, onFocus, onBlur, email, status }) => {
+  const getInputStyle = () => {
+    if (status === "error" || status === "warning") {
+      return "border-(--RED-500) outline-2 outline-solid -outline-offset-2 outline-(--RED-500)";
+    } else {
+      return "border-(--PINK-400)";
+    }
+  };
+  const getAriaDescribedBy = () => {
+    if (status === "error") {
+      return "email-error";
+    }
+    if (status === "success") {
+      return "email-success";
+    }
+  };
+
   return (
     <>
       <label className="sr-only" htmlFor="email">
         Email address
       </label>
       <input
+        className={`w-full rounded-full border border-solid ${getInputStyle()} px-5.5 py-[0.7rem] placeholder:text-sm placeholder:text-(--PINK-400) placeholder:opacity-50 focus-visible:outline-hidden`}
         type="email"
         id="email"
         name="email"
@@ -13,13 +30,7 @@ const InputEmail = ({ onChange, onFocus, onBlur, email, status }) => {
         onFocus={onFocus}
         onBlur={onBlur}
         aria-invalid={status === "error"}
-        aria-describedby={
-          status === "error"
-            ? "email-error"
-            : status === "success"
-              ? "email-success"
-              : undefined
-        }
+        aria-describedby={getAriaDescribedBy()}
         placeholder="Email Address"
       />
     </>

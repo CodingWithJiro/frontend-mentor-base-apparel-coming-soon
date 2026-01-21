dc509e23acd75e5320a65e49ecf9d599f7bf5541
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-21 20:20:04 +0800

    feat(components): create HeroImageMobile component
    This will be the new component replacing HeroImage

---
+++ src/components/HeroImageMobile.jsx
@@ -0,0 +1,13 @@
+const HeroImageMobile = () => {
+  return (
+    <img
+      className="mb-16 w-full md:hidden"
+      src="/src/assets/img/hero-mobile_375x250.jpg"
+      alt=""
+      width="375"
+      height="250"
+    />
+  );
+};
+
+export default HeroImageMobile;

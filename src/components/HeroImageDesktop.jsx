bd2cde9fc1c40def6613cab67991089a1d0712bb
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-21 20:21:00 +0800

    feat(components): create HeroImageDesktop component
    This will be inserted as the right panel element for desktop view

---
+++ src/components/HeroImageDesktop.jsx
@@ -0,0 +1,13 @@
+const HeroImageDesktop = () => {
+  return (
+    <img
+      className="hidden w-full md:block"
+      srcSet="/src/assets/img/hero-desktop_610x800.jpg"
+      alt=""
+      width="610"
+      height="800"
+    />
+  );
+};
+
+export default HeroImageDesktop;

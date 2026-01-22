4e0bd7e93c925f631f54c55be043d0f11ea6ef15
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-23 00:43:03 +0800

    feat(styles): adjust SuccessMessage component padding in desktop view

--- src/components/SuccessMessage.jsx
+++ src/components/SuccessMessage.jsx
@@ -1,7 +1,7 @@
 const SuccessMessage = () => {
   return (
     <p
-      className="absolute -bottom-7 left-0 w-full px-5.5 text-sm tracking-tight text-(--PINK-300)"
+      className="absolute -bottom-7 left-0 w-full px-5.5 text-sm tracking-tight text-(--PINK-300) md:px-8"
       id="email-success"
     >
       Email successfully submitted!

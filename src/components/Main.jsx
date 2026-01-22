848033ae0cd6929ac4a639e4dd5bb5a50465fbce
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-22 22:47:23 +0800

    refactor(components): replace HeroImage with HeroImageMobile and remove BackgroundPattern from Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,5 @@
-import BackgroundPattern from "./BackgroundPattern";
 import Form from "./Form";
-import HeroImage from "./HeroImage";
+import HeroImageMobile from "./HeroImageMobile";
 import MainText from "./MainText";
 
 const Main = () => {
@@ -9,10 +8,9 @@ const Main = () => {
       className="flex flex-col items-center justify-start font-(family-name:--FF)"
       id="main"
     >
-      <HeroImage />
+      <HeroImageMobile />
       <MainText />
       <Form />
-      <BackgroundPattern />
     </main>
   );
 };

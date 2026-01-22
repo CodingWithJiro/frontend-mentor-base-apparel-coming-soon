fbfefff36872a07820edbb5df066ace1226ecdfd
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-22 22:52:44 +0800

    refactor(components): replace img tag with div and background-image combo for HeroImageDesktop component

--- src/components/HeroImageDesktop.jsx
+++ src/components/HeroImageDesktop.jsx
@@ -1,12 +1,8 @@
 const HeroImageDesktop = () => {
   return (
-    <img
-      className="hidden w-full md:block"
-      srcSet="/src/assets/img/hero-desktop_610x800.jpg"
-      alt=""
-      width="610"
-      height="800"
-    />
+    <div className="relative my-auto hidden min-h-[min(50rem,100vh)] w-full max-w-[clamp(20rem,-0.7143rem+43.1548vw,38.125rem)] md:block">
+      <div className="absolute top-0 left-0 h-full w-full bg-[url('/src/assets/img/hero-desktop_610x800.jpg')] bg-cover bg-center"></div>
+    </div>
   );
 };
 

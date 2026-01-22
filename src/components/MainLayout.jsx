e87414ffc3b2c883c9297e2ae680745160955f5e
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-23 00:34:42 +0800

    feat(styles): add max-width to mobile view
    Reason to make the HeroImageMobile component not grow so large that the image quality deteriorates

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,6 +1,6 @@
 const MainLayout = ({ children }) => {
   return (
-    <div className="mx-auto min-h-screen w-full max-w-360 md:flex md:flex-row md:items-center md:justify-center">
+    <div className="mx-auto min-h-screen w-full max-w-115 md:flex md:max-w-360 md:flex-row md:items-center md:justify-center">
       {children}
     </div>
   );

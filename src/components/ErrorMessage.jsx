1a027d5f498dbb9e576d9e6b5b5c22301a4409a7
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-24 02:00:50 +0800

    feat(styles): add motion safe transition to ErrorMessage component

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,7 +1,7 @@
 const ErrorMessage = () => {
   return (
     <p
-      className="md:tracking-tightest absolute -bottom-7 left-0 w-full px-5.5 text-sm tracking-tight text-(--RED-500) md:px-8"
+      className="md:tracking-tightest absolute -bottom-7 left-0 w-full px-5.5 text-sm tracking-tight text-(--RED-500) motion-safe:transition-colors motion-safe:duration-300 motion-safe:ease-in-out md:px-8"
       id="email-error"
       role="alert"
     >

f2c48424f08b30b07db6a8addcbc37e395d2b572
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-24 02:01:19 +0800

    feat(styles): add motion safe transition to SuccessMessage componenet

--- src/components/SuccessMessage.jsx
+++ src/components/SuccessMessage.jsx
@@ -1,7 +1,7 @@
 const SuccessMessage = () => {
   return (
     <p
-      className="absolute -bottom-7 left-0 w-full px-5.5 text-sm tracking-tight text-(--PINK-300) md:px-8"
+      className="absolute -bottom-7 left-0 w-full px-5.5 text-sm tracking-tight text-(--PINK-300) motion-safe:transition-colors motion-safe:duration-300 motion-safe:ease-in-out md:px-8"
       id="email-success"
     >
       Email successfully submitted!

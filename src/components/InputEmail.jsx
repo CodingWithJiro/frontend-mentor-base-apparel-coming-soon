0f293656fe6f2c4886cb834bea9393bd69e8e842
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-24 22:28:41 +0800

    feat(ux): add conditional shake logic to InputEmail component

--- src/components/InputEmail.jsx
+++ src/components/InputEmail.jsx
@@ -1,4 +1,12 @@
-const InputEmail = ({ onChange, onFocus, onBlur, email, status }) => {
+const InputEmail = ({
+  onChange,
+  onFocus,
+  onBlur,
+  onAnimationEnd,
+  email,
+  status,
+  shouldShake,
+}) => {
   const getInputStyle = () => {
     if (status === "error" || status === "warning") {
       return "border-(--RED-500) outline-2 outline-solid -outline-offset-2 outline-(--RED-500)";
@@ -21,7 +29,7 @@ const InputEmail = ({ onChange, onFocus, onBlur, email, status }) => {
         Email address
       </label>
       <input
-        className={`w-full rounded-full border border-solid ${getInputStyle()} px-5.5 py-[0.7rem] placeholder:text-sm placeholder:text-(--PINK-400) placeholder:opacity-50 focus-visible:outline-hidden motion-safe:transition-colors motion-safe:duration-300 motion-safe:ease-in-out motion-safe:placeholder:transition-colors motion-safe:placeholder:duration-300 motion-safe:placeholder:ease-in-out md:px-8 md:py-[0.9rem] md:placeholder:text-[1rem]`}
+        className={`w-full rounded-full border border-solid ${getInputStyle()} ${shouldShake ? "shake" : ""} px-5.5 py-[0.7rem] placeholder:text-sm placeholder:text-(--PINK-400) placeholder:opacity-50 focus-visible:outline-hidden motion-safe:transition-colors motion-safe:duration-300 motion-safe:ease-in-out motion-safe:placeholder:transition-colors motion-safe:placeholder:duration-300 motion-safe:placeholder:ease-in-out md:px-8 md:py-[0.9rem] md:placeholder:text-[1rem]`}
         type="email"
         id="email"
         name="email"
@@ -29,6 +37,7 @@ const InputEmail = ({ onChange, onFocus, onBlur, email, status }) => {
         onChange={onChange}
         onFocus={onFocus}
         onBlur={onBlur}
+        onAnimationEnd={onAnimationEnd}
         aria-invalid={status === "error"}
         aria-describedby={getAriaDescribedBy()}
         placeholder="Email Address"

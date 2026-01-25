d53b2f59952e1c2e14fbc76f84da144633c4d5a5
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-26 00:10:24 +0800

    feat(ux): use error prop in ErrorMessage component to render different messages when error is invalid or duplicate

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,11 +1,12 @@
-const ErrorMessage = () => {
+const ErrorMessage = ({ error }) => {
   return (
     <p
       className="md:tracking-tightest absolute -bottom-7 left-0 w-full px-5.5 text-sm tracking-tight text-(--RED-500) motion-safe:transition-colors motion-safe:duration-300 motion-safe:ease-in-out md:px-8"
       id="email-error"
       role="alert"
     >
-      Please provide a valid email
+      {error === "invalid" && "Please provide a valid email"}
+      {error === "duplicate" && "Email already sent!"}
     </p>
   );
 };

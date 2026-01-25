b3be5ac93d116a3a92d06fc2bc7ee90ab96a6ff3
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-25 23:55:50 +0800

    feat(hooks): import useRef hook in Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import ErrorMessage from "./ErrorMessage";
 import InputEmail from "./InputEmail";
 import SubmitButton from "./SubmitButton";

c8cf0f28b5efa860317d60c83730bdaef728d572
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-27 00:19:48 +0800

    test: add InputEmail component test for disabling input when status is loading

--- src/components/InputEmail.test.jsx
+++ src/components/InputEmail.test.jsx
@@ -1,12 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import InputEmail from "./InputEmail";
 
+const inputRegex = /email address/i;
+
 describe("InputEmail", () => {
   test("adds shake animation when shouldShake is true", () => {
     render(<InputEmail shouldShake={true} />);
 
-    const input = screen.getByLabelText(/email address/i);
+    const input = screen.getByLabelText(inputRegex);
 
     expect(input).toHaveClass("shake");
+  });
+
+  test("disable input element when status is loading", () => {
+    render(<InputEmail status="loading" />);
+
+    const input = screen.getByLabelText(inputRegex);
+
+    expect(input).toBeDisabled();
   });
 });

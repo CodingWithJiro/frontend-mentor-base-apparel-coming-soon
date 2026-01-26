90fd19e7ddabfee1d3a01d8c8b02a00c4fec9ae0
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-26 23:24:17 +0800

    refactor: add aria-hidden true and aria-label to error icon in SubmitButton component

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -27,6 +27,8 @@ const SubmitButton = ({ status, shouldShake, onAnimationEnd }) => {
           xmlns="http://www.w3.org/2000/svg"
           width="24"
           height="24"
+          aria-hidden="true"
+          aria-label="error-icon"
         >
           <g fill="none" fillRule="evenodd">
             <circle cx="12" cy="12" r="12" fill="#F96464" />

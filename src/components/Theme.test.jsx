f0a667bb80b17330f9515cc4b4cc57b3aba14b36
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-28 00:07:29 +0800

    test: add Theme test which checks if theme toggle is applied on button click

--- src/components/Theme.test.jsx
+++ src/components/Theme.test.jsx
@@ -1,4 +1,5 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Theme from "./Theme";
 
 describe("Theme", () => {
@@ -33,5 +34,46 @@ describe("Theme", () => {
     const root = document.documentElement;
 
     expect(root).toHaveClass("dark");
+  });
+
+  test("toggles theme to dark on button click", async () => {
+    window.matchMedia = vi.fn().mockImplementation((query) => ({
+      media: query,
+      matches: false,
+    }));
+
+    const user = userEvent.setup();
+
+    render(<Theme />);
+
+    const root = document.documentElement;
+    const button = screen.getByRole("button");
+
+    expect(root).not.toHaveClass("dark");
+
+    await user.click(button);
+
+    expect(root).toHaveClass("dark");
+  });
+
+  test("toggles theme to light on button click", async () => {
+    window.matchMedia = vi.fn().mockImplementation((query) => ({
+      media: query,
+      matches: false,
+    }));
+
+    localStorage.setItem("theme", "dark");
+    const user = userEvent.setup();
+
+    render(<Theme />);
+
+    const root = document.documentElement;
+    const button = screen.getByRole("button");
+
+    expect(root).toHaveClass("dark");
+
+    await user.click(button);
+
+    expect(root).not.toHaveClass("dark");
   });
 });

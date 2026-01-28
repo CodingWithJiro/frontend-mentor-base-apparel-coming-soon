792a793dcd385309d641d808dccbfd125e7ee44c
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-29 02:49:26 +0800

    refactor: move hero images to public and update src values

--- src/components/HeroImageMobile.jsx
+++ src/components/HeroImageMobile.jsx
@@ -1,10 +1,8 @@
-import heroImageMobile from "../assets/img/hero-mobile_375x250.jpg";
-
 const HeroImageMobile = () => {
   return (
     <img
       className="mb-16 w-full md:hidden"
-      src={heroImageMobile}
+      src="/img/hero-mobile_375x250.jpg"
       alt=""
       width="375"
       height="250"
